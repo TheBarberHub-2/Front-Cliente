5946e59079178ba92443a7243b679729cb0d32f4
PBC5 <160127358+PBC5@users.noreply.github.com>
2026-01-19 08:46:23 +0100

    header y footer

--- Front-Cliente/src/app/app.ts
+++ Front-Cliente/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { CHeader } from './components/ui/c-header/c-header';
+import { CFooter } from './components/ui/c-footer/c-footer';
 @Component({
+  standalone: true,
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CHeader, CFooter],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })

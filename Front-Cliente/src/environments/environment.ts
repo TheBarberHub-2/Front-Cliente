84a892fdf3682966d55302ab86864931cdaa66ed
PBC5 <160127358+PBC5@users.noreply.github.com>
2026-02-13 10:03:00 +0100

    A

--- Front-Cliente/src/environments/environment.ts
+++ Front-Cliente/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://thebarberhub-back.producciondaw.cip.fpmislata.com/api' // Cambiar aquí para producción: 'https://producciondaw.cip.fpmislata.com/api'
+    apiUrl: 'http://producciondaw.cip.fpmislata.com/api' // Cambiar aquí para producción: 'https://producciondaw.cip.fpmislata.com/api'
 };
